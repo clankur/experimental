ed132f33822a92829f0991424f256437d862eab7
Ankur Mishra <2018ankurm@gmail.com>
2025-01-13 16:52:10 -0500

    focus on 250 runs

--- docs/lora.py
+++ docs/lora.py
@@ -2,8 +2,12 @@
 from plot_helper import *
 
 # %%
-url = "https://app.clear.ml/projects/ce508798b5384eeea472bf6b5357929b/compare-experiments;ids=483668aecfb34f0e98ab13a69270c31f,d4122017e5814f0d94102b8db258b149,8607ffb881a9418cb8276e820e7cf8c5,cb20112968a44b9387f9a5e404f22942,0d26acce03fc46659b7e94ef0f2d4931,c049a27419d84cadb3b6fade2d057051/details"
+url = "https://app.clear.ml/projects/ce508798b5384eeea472bf6b5357929b/compare-experiments;ids=483668aecfb34f0e98ab13a69270c31f,d4122017e5814f0d94102b8db258b149,8607ffb881a9418cb8276e820e7cf8c5,cb20112968a44b9387f9a5e404f22942/details"
 experiment_ids = get_experiment_ids_from_url(url)
 # %%
-experiment_ids
+loss_data = get_loss_data(experiment_ids)
+[(k, v["name"]) for (k, v) in loss_data.items()]
+# %%
+plot_loss_data(loss_data, plot_last=125)
+
 # %%
